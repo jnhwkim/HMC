1545afa31438176d16a781523cad27e5d19485c3
Jin Hwa Kim <jnhwkim@gmail.com>
2015-01-07 08:49:40 +0900

    500x500 NN outputs 93.02%.

--- caffe/caffe_test.m
+++ caffe/caffe_test.m
@@ -5,14 +5,15 @@ addpath('/Users/Calvin/Github/caffe-master/matlab/caffe');
 if caffe('is_initialized')
     caffe('reset');
 end
-resample = 8;
+samples = 49;
+resample = 28;
 use_gpu = 1;
-model_def_file = sprintf('hmc_deploy_%ds.prototxt',resample);
-model_file = sprintf('.%ds_ga_iter_10000000.caffemodel',resample);
+model_def_file = sprintf('hmc_deploy_%ds%dr_wide.prototxt',samples,resample);
+model_file = sprintf('.%ds%dr_wide_iter_10000000.caffemodel',samples,8);
 matcaffe_init(use_gpu, model_def_file, model_file);
 
 %% load test data
-load(sprintf('../mat/%dS_te.mat',resample));
+load(sprintf('../mat/%ds%dr_te.mat',samples,resample));
 
 %% Load MNIST data
 addpath('../mnist');
@@ -21,16 +22,25 @@ y_te = loadMNISTLabels('t10k-labels-idx1-ubyte');
 %% forward
 correct = 0;
 count = 0;
-for idx = 1 : 10000
+error = zeros(1,10);
+
+for idx = 1 : size(S_te,1)/resample
     X = zeros(size(S_te(1,:), 2), ...
               size(S_te(1,:), 1), 1, resample);
     X(:,1,1,:) = S_te(resample*(idx-1)+1:resample*(idx-1)+resample,:)';
-    input_data = {single(X)};
+    input_data = {single(uint8(X*255))*0.00390625};
 
     scores = caffe('forward', input_data);
+    
     [~,maxlabel] = max(scores{1});
     if mode(maxlabel-1) == y_te(idx)
         correct = correct + 1;
+    else 
+        if y_te(idx) == 0
+            error(1,10) = error(1,10) + 1;
+        else
+            error(1,y_te(idx)) = error(1,y_te(idx)) + 1;
+        end
     end
     count = count + 1;
 end
