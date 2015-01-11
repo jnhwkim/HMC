7b2dce7c6f0ae5e09d3e475c66b95cef79a93f99
Jin Hwa Kim <jnhwkim@gmail.com>
2015-01-11 10:00:50 +0900

    Updated test script.

--- caffe/caffe_test.m
+++ caffe/caffe_test.m
@@ -6,31 +6,38 @@ if caffe('is_initialized')
     caffe('reset');
 end
 samples = 49;
-resample = 8;
+resample_tr = 8;
+resample_te = 40;
 use_gpu = 1;
-variation = 'z';
+
+%% Model Variation
+variation = 'zj';
+model_def_variation = 'zw';
+model_variation = 'zwj';
+
+%% Inputs
 model_def_file = sprintf('hmc_deploy_%ds%dr%s.prototxt', ...
-                    samples,resample,variation);
+                    samples,resample_te,model_def_variation);
 model_file = sprintf('.%ds%dr%s_iter_10000000.caffemodel', ...
-                    samples,8,variation);
+                    samples,resample_tr,model_variation);
 matcaffe_init(use_gpu, model_def_file, model_file);
 
-%% load test data
-load(sprintf('../mat/%ds%dr%s_te.mat',samples,resample,variation));
+%% Load test data
+load(sprintf('../mat/%ds%dr%s_te.mat',samples,resample_te,variation));
 
 %% Load MNIST data
 addpath('../mnist');
 y_te = loadMNISTLabels('t10k-labels-idx1-ubyte');
 
-%% forward
+%% Forward
 correct = 0;
 count = 0;
 error = zeros(1,10);
 
-for idx = 1 : size(S_te,1)/resample
+for idx = 1 : size(S_te,1)/resample_te
     X = zeros(size(S_te(1,:), 2), ...
-              size(S_te(1,:), 1), 1, resample);
-    X(:,1,1,:) = S_te(resample*(idx-1)+1:resample*(idx-1)+resample,:)';
+              size(S_te(1,:), 1), 1, resample_te);
+    X(:,1,1,:) = S_te(resample_te*(idx-1)+1:resample_te*(idx-1)+resample_te,:)';
     input_data = {single(uint8(X*255))*0.00390625};
 
     scores = caffe('forward', input_data);
